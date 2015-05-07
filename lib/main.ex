e9d0c9afe5d72e08f79d49b234758c02b2b15599
Matt Robinson <matt@mattrobinson.net>
2015-05-06 20:09:34 -0700

    wip

--- lib/main.ex
+++ lib/main.ex
@@ -26,8 +26,8 @@ defmodule ZeePipe do
   def min_max_mean_weights do
     f = File.stream!('mss.csv')
 
-    weights = f |> Stream.map( fn(x) -> String.split(x, ~r/\",\"/) end)
-                |> Stream.map( fn(x) -> {:ok, weight} = Enum.fetch(x, 12); String.to_float(weight) end)
+    weights = f |> pmap( fn(x) -> String.split(x, ~r/\",\"/) end)
+                |> pmap( fn(x) -> {:ok, weight} = Enum.fetch(x, 12); String.to_float(weight) end)
 
     min_weight = Enum.min(weights)
     max_weight = Enum.max(weights)
@@ -48,5 +48,31 @@ defmodule ZeePipe do
   defp count(list, gender) do
     list |> Enum.filter( fn(x) -> String.match?(x, ~r/"#{gender}/) end )
          |> Enum.count
+  end
+
+  def pmap(collection, function) do
+    # Get this process's PID
+    me = self
+    collection
+    |>
+    Enum.map(fn (elem) ->
+      # For each element in the collection, spawn a process and
+      # tell it to:
+      # - Run the given function on that element
+      # - Call up the parent process
+      # - Send the parent its PID and its result
+      # Each call to spawn_link returns the child PID immediately.
+      spawn_link fn -> (send me, { self, function.(elem) }) end
+    end) |>
+    # Here we have the complete list of child PIDs. We don't yet know
+    # which, if any, have completed their work
+    Enum.map(fn (pid) ->
+      # For each child PID, in order, block until we receive an
+      # answer from that PID and return the answer
+      # While we're waiting on something from the first pid, we may
+      # get results from others, but we won't "get those out of the
+      # mailbox" until we finish with the first one.
+      receive do { ^pid, result } -> result end
+    end)
   end
 end
