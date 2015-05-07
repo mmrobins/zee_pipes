b221f626fffd8de71cf88690497a97c138850b29
Matt Robinson <matt@mattrobinson.net>
2015-05-06 19:25:18 -0700

    wip

--- lib/main.ex
+++ lib/main.ex
@@ -10,13 +10,18 @@ defmodule ZeePipe do
     female_count = count(with_seq, "female")
     IO.puts("female count")
     IO.inspect(female_count)
+    IO.puts("female ratio")
+    IO.inspect(female_count / total)
 
     male_count = count(with_seq, "male")
     IO.puts("male count")
     IO.inspect(male_count)
+    IO.puts("male ratio")
+    IO.inspect(male_count / total)
     Enum.count(with_seq)
   end
-                                                                                                         def count(list, gender) do
+
+  def count(list, gender) do
     list |> Enum.map( fn(x) -> String.split(x, ~r/\",\"/) end)
         #|> Enum.filter( fn(x) -> foo = hd(tl(x)); IO.inspect(foo); IO.inspect(is_bitstring(foo)); to_string(foo) == "female" end)
          |> Enum.filter( fn(x) -> foo = hd(tl(x)); to_string(foo) == gender end)
